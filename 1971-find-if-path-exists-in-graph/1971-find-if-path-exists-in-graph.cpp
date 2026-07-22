a30aef5ca4e4b49e88769a1760919983eac2bcbc
Amit Kumar <amitsah2319@gmail.com>
2026-07-22 22:03:02 +0530

    Time: 368 ms (41.25%), Space: 311.9 MB (38.21%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    
-    bool dfs(int source , int destination, vector<vector<int>>& adj, vector<bool>&vis){
-        if(source == destination )return true;
-        vis[source]=true;
-        for(int neighbour : adj[source]){
-            if(!vis[neighbour]){
-                if(neighbour ==destination || dfs(neighbour, destination , adj, vis)){
-                    return true;
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+       // adjlist
+        vector<vector<int>>adjList(n);
+        for(auto edge:edges){
+            adjList[edge[0]].push_back(edge[1]);
+            adjList[edge[1]].push_back(edge[0]);
+        }
+         // bfs traversal
+        queue<int>q;
+        vector<bool>visited(n, false);
+        q.push(source);
+        visited[source]=true;
+        while(!q.empty()){
+            int temp = q.front();
+            q.pop();
+            if(temp==destination)return true;
+            for(int i=0;i<adjList[temp].size();i++){
+                if(!visited[adjList[temp][i]]){
+                    q.push(adjList[temp][i]);
+                    visited[adjList[temp][i]]=true;
                 }
             }
         }
         return false;
-
-    }
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        // adj list 
-      vector<vector<int>>adj(n);
-      for(auto&e: edges){
-        adj[e[0]].push_back(e[1]);
-        adj[e[1]].push_back(e[0]);
-      }
-      vector<bool>vis(n,false);
-      return dfs(source , destination , adj , vis);
     }
 };
